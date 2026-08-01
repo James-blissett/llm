d21a081d807f61dabf5cde166b8e4dcdb34cbbde
James Blissett <jamesdblissett@outlook.com>
2026-08-01 13:00:03 +1000

    finished data processing infra

--- data.py
+++ data.py
@@ -194,3 +194,32 @@ data_iter = iter(dataloader)
 first_batch = next(data_iter)
 print(first_batch)
 
+
+# ---------------------------------------------------------------------------
+# Creating token embeddings
+# ---------------------------------------------------------------------------
+
+inputs_ids = torch.tensor([2, 3, 5, 1])
+
+vocab_size = 6
+output_dim = 3
+
+torch.manual_seed(123)
+
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+print(embedding_layer.weight)
+
+# ---------------------------------------------------------------------------
+# Encode word positions
+# ---------------------------------------------------------------------------
+vocab_size = 50257
+output_dim = 256
+
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+max_length = 4
+dataloader = create_dataloader_v1(raw text, batch_size=8, max_length = max_length, stride=max_length, shuffle=False)
+
+data_itr = iter(dataloader)
+inputs, targets = next(data_itr)
